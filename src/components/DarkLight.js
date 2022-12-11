ed1324a74663b6ce76a11e424d648917d62da1d2
Lianne Dakanno <liannedakanno@gmail.com>
2022-12-11 15:25:51 -0600

    create db.json

---
+++ src/components/DarkLight.js
@@ -0,0 +1,5 @@
+export default function DarkLight() {
+  return (
+    <div>DarkLight</div>
+  )
+}
---
+++ src/components/PlantCard.js
@@ -0,0 +1,5 @@
+export default function PlantCard() {
+  return (
+    <div>PlantCard</div>
+  )
+}
