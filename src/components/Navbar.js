24b332ceec86e702f605308205c70e964b539916
Lianne Dakanno <liannedakanno@gmail.com>
2022-12-11 16:55:59 -0600

    create navlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,55 @@
+import {NavLink} from 'react-router-dom';
+
+const style= {
+  width: "60%",
+  margin: "5% 0 1%",
+  padding: "1em"
+}
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <NavLink
+        activeStyle={{
+          fontweight: "bolder",
+          color: "red"
+        }}
+        exact
+        style={style}
+        to="/"
+      >Home
+      </NavLink>
+      <NavLink
+        activeStyle={{
+          fontweight: "bolder",
+          color: "red"
+        }}
+        exact
+        style={style}
+        to="/"
+      >About
+      </NavLink>
+      <NavLink
+        activeStyle={{
+          fontweight: "bolder",
+          color: "red"
+        }}
+        exact
+        style={style}
+        to="/"
+      >Search
+      </NavLink>
+      <NavLink
+        activeStyle={{
+          fontweight: "bolder",
+          color: "red"
+        }}
+        exact
+        style={style}
+        to="/"
+      >Comment
+      </NavLink>
+    </div>
   )
 }
 
