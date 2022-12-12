40f6328f95ea0200734ca29b91593796f0fa5029
Lianne Dakanno <liannedakanno@gmail.com>
2022-12-11 20:14:24 -0600

    add h3 with info

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@
 
 const About = () => {
   return (
-    <div></div>
+    <div>
+      <h3 style={{ textAlign: "center", margin: "3rem", fontWeight: ""}}>
+        The aloe vera plant is an easy, attractive succulent that makes for a great indoor companion. Aloe vera plants are useful, too, as the juice from their leaves can be used to relieve pain from scrapes and burns when applied topically.
+      </h3>
+    </div>
   )
 }
 
