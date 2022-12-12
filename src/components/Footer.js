27826f4d8fffd6fdd36933bde91ef70e737b39a2
Lianne Dakanno <liannedakanno@gmail.com>
2022-12-11 20:45:00 -0600

    change color to green

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div style={{display: "flex", textAlign: "center", backgroundColor: "grey", color: "black"}}>
+    <div style={{display: "flex", textAlign: "center", backgroundColor: "green", color: "black"}}>
       <p style={{width: "50%", textAlign: "center"}}>Thanks for visiting!</p>
     </div>
   )
