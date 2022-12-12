4935df3e23559a86f57b15c5c87bccb951313a5f
Lianne <104999175+Ldakanno@users.noreply.github.com>
2022-12-12 00:22:07 -0600

    Delete Footer.js

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div style={{display: "flex", textAlign: "center", backgroundColor: "green", color: "black"}}>
-      <p style={{width: "50%", textAlign: "center"}}>Thanks for visiting!</p>
-    </div>
-  )
-}
-
-export default Footer
