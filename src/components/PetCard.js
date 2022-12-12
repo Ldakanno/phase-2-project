36c4f1eb905f16d57a8c56e88feb0b7edf1de513
Lianne Dakanno <liannedakanno@gmail.com>
2022-12-12 00:08:29 -0600

    create pet card functionality

---
+++ src/components/PetCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function PetCard({ petObj, children }) {
+    const {name, image, sex, animal, id} = petObj;
+
+    return (
+        <div id={id} className='petcard'>
+            <img src={image} alt={name} className='takeme' />
+            <h2>{name}</h2>
+            <p>Type of Animal: {animal}</p>
+            <p>Sex: {sex}</p>
+            <button>
+                <Link className='cardLinks' to={`/pets/${id}`}>Learn More</Link>
+            </button>
+            <br />
+            <br />
+            {children}
+        </div>
+    )
+}
+
+export default PetCard;
+
