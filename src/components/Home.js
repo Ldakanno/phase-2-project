d20a05ef1c21792370a76d05435ac99b563bc9ce
Lianne Dakanno <liannedakanno@gmail.com>
2022-12-11 20:14:51 -0600

    add image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 const Home = () => {
   return (
-    <div></div>
+    <div>
+      <img style={{ textAlign: "center", width: "100px", height: "" }}src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/aloe-vera-plant-outside-jpg-1522875135.jpg?crop=1xw:1xh;center,top&resize=980:*"/>
+    </div>
   )
 }
 
