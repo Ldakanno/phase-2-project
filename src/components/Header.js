19fbe6767ffdb065f644449de555847f388e9192
Lianne Dakanno <liannedakanno@gmail.com>
2022-12-12 00:17:14 -0600

    add text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,30 @@
-const Header = ( {greeting, title} ) => {
-  return (
-    <div>
-      <h1>{title}</h1>
-      <h3>{greeting}</h3>
-    </div>
-  )
+import SearchPets from "./SearchPets";
+import { Link, useHistory } from 'react-router-dom';
+
+// import Home from './Home';
+
+function Header({ searchPet, handleSearch }) {
+    let history = useHistory();
+
+    return (
+        <div className='static'>
+            <img src='https://www.jfphonline.com/storage/app/media/hp-animals.png' alt='pet-group' />
+            <h1 className="header">Pet Adoption</h1>
+            {/* <img src='https://www.jfphonline.com/storage/app/media/hp-animals.png' alt='pet-group' /> */}
+            <div className='home'>
+                <h2>
+                    <Link className='links' to='/' onClick={() => history.push('/')}>All Pets</Link>
+                </h2>
+            </div>
+            <div className='home'>
+                <h2>
+                    <Link className='links' to='/pets/mine' onClick={() => history.push('/pets/mine')}>My Pets</Link>
+                </h2>
+            </div> <br /><br />
+            <SearchPets searchPet={searchPet} handleSearch={handleSearch} />
+            {/* <img class="logo" src={Logo} alt="logo"/> */}
+        </div>
+    )
 }
 
-export default Header
+export default Header;
